c04dfe1d1c135ba84648ca3c96f986fcba8a9c15
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-21 21:46:27 +0530

    Add files via upload

--- src/components/element.js
+++ src/components/element.js
@@ -1,63 +1,6 @@
-import React, { Component } from 'react';
-
-class Element extends Component {
-  render() {
-    return (
-      <div className="element" value={this.props.value}>
-        {this.props.value}
-      </div>
-    );
-  }
-}
-
-class Stack extends Component {
-  constructor() {
-    super();
-    this.state = {
-      stack: [],
-    };
-  }
-
-  push = (value) => {
-    let newStack = this.state.stack;
-    newStack.push(value);
-    this.setState({ stack: newStack });
-  };
-
-  pop = () => {
-    let newStack = this.state.stack;
-    if (newStack.length === 0) {
-      window.alert("Stack is empty");
-      return;
-    }
-    let value = newStack.pop();
-    this.setState({ stack: newStack });
-    return value;
-  };
-
-  top = () => {
-    let newStack = this.state.stack;
-    if (newStack.length === 0) {
-      window.alert("Stack is empty");
-      return;
-    }
-    let value = newStack[newStack.length - 1];
-    return value;
-  };
-
-  render() {
-    const stack = this.state.stack.map(val => {
-      return <Element value={val} key={val} />;
-    });
-    return (
-      <div className="stack">
-        {stack}
-        <button onClick={() => this.push(this.props.value)}>Push</button>
-        <button onClick={() => this.pop()}>Pop</button>
-        <button onClick={() => this.top()}>Top</button>
-      </div>
-    );
-  }
+import React from 'react';
+function Element(props) {
+  return <div className="element">{props.item}</div>;
 }
 
 export default Element;
